6e3f1df41df1b8dbaa4461fc6657b577a3da4fae
deprussian <dmitrii.tsikh@gmail.com>
2025-02-22 23:25:12 +0500

    second

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Sum: {a + b}");
+        Console.WriteLine($"Difference: {a - b}");
+        Console.WriteLine($"Product: {a * b}");
+        Console.WriteLine($"Quotient: {a / b}");
+    }
+}
