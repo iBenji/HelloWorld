d669c4f6705ab642fd5e9b88ff7dc4050306567e
deprussian <dmitrii.tsikh@gmail.com>
2025-02-22 23:33:07 +0500

    lol

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        try 
+        {
         Console.WriteLine("Введите первое число");
         int a = int.Parse(Console.ReadLine());
         Console.WriteLine("Введите второе число");
@@ -22,6 +24,20 @@ class Program
         else
         {
             Console.WriteLine("Деление на ноль невозможно");
+        }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: Введено не число.");
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sum: {a + b}");
+            Console.WriteLine($"Difference: {a - b}");
+            Console.WriteLine($"Product: {a * b}");
+            Console.WriteLine($"Quotient: {a / b}");
         }
     }
 }
